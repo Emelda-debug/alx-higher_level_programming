53d12b3e42a865fccbf57ee1ba5d808953f56342
Emelda-debug <emeldamada4@gmail.com>
2023-12-01 03:08:56 +0000

    more corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     elif x == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(x))
     for i in range(x):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     rslt = 0
-    for i in range(len(sys.argv) - 1:
+    for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
     print("{}".format(rslt))
 
