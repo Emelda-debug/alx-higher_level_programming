63a856a0ce427e820916f76cbb8553f529005a38
Emelda-debug <emeldamada4@gmail.com>
2024-01-05 19:20:54 +0000

    mandatory tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" class with no class or object attribute """
+class LockedClass:
+    """ prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name"""
+    __slots__ = ["first_name"]
