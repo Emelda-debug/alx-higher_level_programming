eff0758e23d62b875543b35b6d1bec19020a209c
Emelda-debug <emeldamada4@gmail.com>
2024-01-19 16:36:45 +0000

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,9 @@ import turtle
 
 class Base:
 """ class Base with private class attribute  __nb_objects = 0 and class constructor def __init__(self, id=None)"""
+
 __nb_objects = 0
+
 def __init__(self, id=None):
     """ Initialise a new Base
     Args:
@@ -60,4 +62,114 @@ def create(cls, **dictionary):
     ** dictionary: double pointer to a dictionary
     """
     if dictionary and dictionary != {}:
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        else:
+            new = cls(1)
+            new.update(**dictionary)
+            return new
+
+@classmethod
+def load_from_file(cls):
+    """ Return classes instantiated from a file of JSON strings
+    Reads from `<cls.__name__>.json`.
+    Returns:
+    empty list if the file does not exist
+    Otherwise a list of instantiated classes
+    """
+    filename = str(cls.__name__) + ".json"
+    try:
+        with open(filename, "r") as jsonfile:
+            list_dicts = Base.from_json_string(jsonfile.read())
+            return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+@classmethod
+def save_to_file_csv(cls, list_objs):
+    """ Write CSV serialization of a list of objects to a file
+    Args:
+    list_objs: A list of inherited Base instances
+    """
+    filename = cls.__name__ + ".csv"
+    with open(filename, "w", newline="") as csvfile:
+        if list_objs is None or list_objs == []:
+            csvfile.write("[]")
+        else:
+            if cls.__name__ == "Rectangle":
+                fieldnames = ["id", "width", "height", "x", "y"]
+            else:
+                fieldnames = ["id", "size", "x", "y"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                for obj in list_objs:
+                    writer.writerow(obj.to_dictionary())
+
+@classmethod
+def load_from_file_csv(cls):
+    """ Return classes instantiated from a CSV file
+    Reads from `<cls.__name__>.csv`
+    Returns:
+    empty list if the file does not exist
+    Otherwise a list of instantiated classes
+    """
+    filename = cls.__name__ + ".csv"
+    try:
+        with open(filename, "r", newline="") as csvfile:
+            if cls.__name__ == "Rectangle":
+                fieldnames = ["id", "width", "height", "x", "y"]
+            else:
+                fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                        for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+            except IOError:
+                return []
+
+@staticmethod
+def draw(list_rectangles, list_squares):
+    """ Draw rectangkes and Squares using the turtle module
+    Args:
+    list_rectangles: A list of Rectangle objects to draw
+    list_squares: A list of Square objects to draw
+    """
+    turt = turtle.Turtle()
+    turt.screen.bgcolor("#ffffff")
+    turt.pensize(3)
+    turt.shape("turtle")
+
+    turt.color("#000000")
+    for rect in list_rectangles:
+        turt.showturtle()
+        turt.up()
+        turt.goto(rect.x, rect.y)
+        turt.down()
+        for x in range(2):
+           turt.forward(rect.width)
+           turt.left(90)
+           turt.forward(rect.height)
+           turt.left(90)
+           turt.hideturtle()
+
+        turt.color("#b5e3d8")
+        for s in list_squares:
+            turt.showturtle()
+            turt.up()
+            turt.goto(sq.x, sq.y)
+            turt.down()
+           for x in range(2):
+               turt.forward(s.width)
+               turt.left(90)
+               turt.forward(sq.height)
+               turt.left(90)
+            turt.hideturtle()
+
+        turtle.exitonclick()
+
+
+
+
+
+
+
 
