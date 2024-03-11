1b076de1a8ff7a877b659f0d1391a6347c70e2da
Emelda-debug <emeldamada4@gmail.com>
2024-03-11 16:19:00 +0000

    tasks

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const MultiLanguagesLoop = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let x = 0; x < MultiLanguagesLoop.length; x++) {
+  console.log(MultiLanguagesLoop[x]);
+}
